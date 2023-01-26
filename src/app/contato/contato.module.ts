ff2f18ea48077c040b4dcbb8bdf661bf85fdf7bb
Jessica <jessiamorim25@gmail.com>
2023-01-25 23:51:37 -0300

    build

---
+++ src/app/contato/contato.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ContatoRoutingModule } from './contato-routing.module';
+import { ContatoComponent } from './contato.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskPipe } from 'ngx-mask';
+
+
+
+
+@NgModule({
+  declarations: [
+    ContatoComponent
+  ],
+  imports: [
+    CommonModule,
+    ContatoRoutingModule,
+    ReactiveFormsModule,
+    NgxMaskPipe
+  ]
+})
+export class ContatoModule { }
